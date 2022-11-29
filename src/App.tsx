23590b8b7d08fb992c0460e9db85154fc10aa7b1
Gustavo Diniz <gustavo.diniz@pixelplan.net>
2022-11-29 20:13:32 +0000

    Router dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div className="App">
-        <Button variant="danger" />
-      </div>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
-  );
+  )
 }
