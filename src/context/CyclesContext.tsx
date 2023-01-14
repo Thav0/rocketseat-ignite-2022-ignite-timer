27c8564b3f2f12bbd967d9b740b922ff5bccd26e
Gustavo Diniz <gustavo.diniz@pixelplan.net>
2023-01-14 18:54:37 +0000

    Reset

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -15,6 +15,7 @@ interface Cycle {
 }
 
 interface CyclesContextType {
+  cycles: Cycle[]
   activeCycle: Cycle | undefined
   activeCycleId: string | null
   amountSecondsPassed: number
@@ -66,8 +67,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(generatedId)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
@@ -86,6 +85,7 @@ export function CyclesContextProvider({
   return (
     <CyclesContext.Provider
       value={{
+        cycles,
         activeCycle,
         activeCycleId,
         maskCurrentCycleAsFinished,
